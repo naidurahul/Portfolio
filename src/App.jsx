ea637864abdf8f732d6fab1d18e4dbaab3419dca
Rahul naidu <first3843@gmail.com>
2023-07-11 17:49:47 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import About from "./components/About";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <About />
+      <Navbar />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data.jsx
@@ -0,0 +1,43 @@
+export const skills = ["javascript", "react", "TailwindCSS", "NodeJS"];
+export const projects = [
+  {
+    title: "Blood bank",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./blood.gif",
+  },
+  {
+    title: "Counter App",
+    subtitle: "ReactJS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  },
+  {
+    title: "TODO App",
+    subtitle: "ReactJS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  },
+];
+
+export const testimonials = [
+    {
+        quote: "Iam impressed",
+        name: "David",
+        company: "xyz",
+        image:"./pic.jpg",
+    },
+    {
+        quote: "Nice",
+        name: "king",
+        company: "Meta-lite",
+        image:"./pic1.jpg",
+    },
+    {
+        quote: "impressive",
+        name: "liza",
+        company: "Ungo-base",
+        image:"./pic2.jpg",
+    },
+]
